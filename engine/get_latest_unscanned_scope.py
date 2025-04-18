99aa6566035aa1c76a63adf5bf0836307f3c334d
Steph <stephanzag@gmail.com>
2025-04-18 23:00:03 +0200

    Update get_latest_unscanned_scope.py

--- engine/get_latest_unscanned_scope.py
+++ engine/get_latest_unscanned_scope.py
@@ -1,20 +1,35 @@
 from db import Scope, session
 from datetime import datetime, timezone
-
+from sqlalchemy import and_
 def utc_now():
     return datetime.now(timezone.utc)
 
 def get_latest_unscanned_or_oldest_scanned():
  
-    oldest_scanned = session.query(Scope).order_by(Scope.updated_at.asc()).first()
+    latest_unscanned = (
+        session.query(Scope)
+        .filter(and_(Scope.type == "domains", Scope.scanned == False))
+        .order_by(Scope.created_at.asc())
+        .first()
+    )
 
-    if oldest_scanned:
-        print(f"LATEST_SCOPE_ID={oldest_scanned.id}")
-        print(f"LATEST_SCOPE_NAME={oldest_scanned.name}")
-        print(f"LATEST_SCOPE_TYPE ={oldest_scanned.type}")
-        oldest_scanned.updated_at = utc_now()
+    if not latest_unscanned:
+        latest_unscanned = (
+            session.query(Scope)
+            .filter(Scope.type == "domains")
+            .order_by(Scope.updated_at.asc())
+            .first()
+    )
+
+
+    if latest_unscanned:
+        print(f"LATEST_SCOPE_ID={latest_unscanned.id}")
+        print(f"LATEST_SCOPE_NAME={latest_unscanned.name}")
+        print(f"LATEST_SCOPE_TYPE ={latest_unscanned.type}")
+        latest_unscanned.updated_at = utc_now()
+        latest_unscanned.scanned = True
         session.commit()
-        return oldest_scanned
+        return latest_unscanned
     else:
         print("LATEST_SCOPE_ID=None")
         print("LATEST_SCOPE_NAME=None")
